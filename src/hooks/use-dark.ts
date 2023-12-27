a1a572bb4ebed82117380cc7fc63b20c1044a1f7
Stephen Zhou <hi@hyoban.cc>
2023-12-27 14:37:31 +0800

    refactor: use createIsDarkAtom

--- src/hooks/use-dark.ts
+++ src/hooks/use-dark.ts
@@ -1,6 +1,7 @@
 import { atom, useAtom, useAtomValue, useSetAtom } from "jotai"
 import { atomEffect } from "jotai-effect"
 import { atomWithStorage } from "jotai/utils"
+import { useMemo } from "react"
 
 const isSystemDarkAtom = atom<boolean | null>(null)
 isSystemDarkAtom.onMount = (set) => {
@@ -22,43 +23,53 @@ function isDarkMode(setting?: Theme | null, isSystemDark?: boolean | null) {
   return setting === "dark" || (isSystemDark && setting !== "light")
 }
 
-const themeAtom = atomWithStorage<Theme>("use-dark", "system")
+function createIsDarkAtom(storageKey: string) {
+  const themeAtom = atomWithStorage<Theme>(storageKey, "system")
 
-const isDarkAtom = atom((get) => {
-  const theme = get(themeAtom)
-  const isSystemDark = get(isSystemDarkAtom)
-  return isDarkMode(theme, isSystemDark)
-})
+  const isDarkAtom = atom((get) => {
+    const theme = get(themeAtom)
+    const isSystemDark = get(isSystemDarkAtom)
+    return isDarkMode(theme, isSystemDark)
+  })
 
-const toggleDarkAtom = atom(null, (get, set) => {
-  const theme = get(themeAtom)
-  const isSystemDark = get(isSystemDarkAtom)
-  if (theme === "system") {
-    set(themeAtom, isSystemDark ? "light" : "dark")
-  } else {
-    set(themeAtom, "system")
-  }
-})
+  const toggleDarkAtom = atom(null, (get, set) => {
+    const theme = get(themeAtom)
+    const isSystemDark = get(isSystemDarkAtom)
+    if (theme === "system") {
+      set(themeAtom, isSystemDark ? "light" : "dark")
+    } else {
+      set(themeAtom, "system")
+    }
+  })
 
-const toggleDarkEffect = atomEffect((get, set) => {
-  const isDark = get(isDarkAtom)
-  if (isDark) {
-    document.documentElement.classList.toggle("dark", true)
-  } else {
-    document.documentElement.classList.toggle("dark", false)
-  }
+  const toggleDarkEffect = atomEffect((get, set) => {
+    const isDark = get(isDarkAtom)
+    if (isDark) {
+      document.documentElement.classList.toggle("dark", true)
+    } else {
+      document.documentElement.classList.toggle("dark", false)
+    }
 
-  const theme = get(themeAtom)
-  const isSystemDark = get(isSystemDarkAtom)
-  if (
-    (theme === "dark" && isSystemDark) ||
-    (theme === "light" && !isSystemDark)
-  ) {
-    set(themeAtom, "system")
-  }
-})
+    const theme = get(themeAtom)
+    const isSystemDark = get(isSystemDarkAtom)
+    if (
+      (theme === "dark" && isSystemDark) ||
+      (theme === "light" && !isSystemDark)
+    ) {
+      set(themeAtom, "system")
+    }
+  })
 
-export function useDark() {
+  return { isDarkAtom, toggleDarkAtom, toggleDarkEffect }
+}
+
+export const useSystemDark = () => useAtomValue(isSystemDarkAtom)
+
+export function useDark(storageKey = "use-dark") {
+  const { isDarkAtom, toggleDarkAtom, toggleDarkEffect } = useMemo(
+    () => createIsDarkAtom(storageKey),
+    [storageKey],
+  )
   const isDark = useAtomValue(isDarkAtom)
   const toggleDark = useSetAtom(toggleDarkAtom)
   useAtom(toggleDarkEffect)
