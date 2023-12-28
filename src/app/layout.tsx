afecc341b0e089d1ddf60f409e831f4d8a8e9e76
Stephen Zhou <hi@hyoban.cc>
2023-12-28 19:37:57 +0800

    refactor: move sonner out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,14 @@ import "@unocss/reset/tailwind.css"
 import "virtual:uno.css"
 import "~/lib/i18n"
 
+import { useDark } from "jotai-dark"
+import { Toaster } from "sonner"
 import { SWRConfig } from "swr"
 
 import { TailwindIndicator } from "~/components/tailwind-indicator"
-import { Toaster } from "~/components/ui/sonner"
 
 export default function Layout({ children }: React.PropsWithChildren) {
+  const { isDark } = useDark()
   return (
     <SWRConfig
       value={{
@@ -15,7 +17,21 @@ export default function Layout({ children }: React.PropsWithChildren) {
       }}
     >
       {children}
-      <Toaster />
+      <Toaster
+        theme={isDark ? "dark" : "light"}
+        className="toaster group"
+        toastOptions={{
+          classNames: {
+            toast:
+              "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            description: "group-[.toast]:text-muted-foreground",
+            actionButton:
+              "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+            cancelButton:
+              "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+          },
+        }}
+      />
       <TailwindIndicator />
     </SWRConfig>
   )
