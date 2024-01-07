9cb5534540db818e86b7e17732524d33101ddb1c
Stephen Zhou <hi@hyoban.cc>
2024-01-07 18:19:33 +0800

    chore: add jotai-query demo

--- src/app/(nav)/analytics/page.tsx
+++ src/app/(nav)/analytics/page.tsx
@@ -1,7 +1,44 @@
+import { atom, useAtomValue } from "jotai"
+import { atomWithSuspenseQuery } from "jotai-tanstack-query"
+import { Suspense } from "react"
+
+import { Loading } from "~/components/loading"
 import { twx } from "~/lib/utils"
+
+import type { Getter } from "jotai"
 
 const Card = twx.div`rounded-lg border bg-card text-card-foreground shadow-sm p-4`
 
+const idAtom = atom(1)
+const userAtom = atomWithSuspenseQuery((get: Getter) => ({
+  queryKey: ["users", get(idAtom)],
+  queryFn: async ({ queryKey: [, id] }) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+    return res.json() as Promise<{
+      userId: number
+      id: number
+      title: string
+      completed: boolean
+    }>
+  },
+}))
+
+const UserData = () => {
+  const { data, isError } = useAtomValue(userAtom)
+  if (isError) return <div>Error</div>
+  return (
+    <pre className="overflow-auto">
+      <code>{JSON.stringify(data, null, 2)}</code>
+    </pre>
+  )
+}
+
 export default function Page() {
-  return <Card>analytics</Card>
+  return (
+    <Card>
+      <Suspense fallback={<Loading />}>
+        <UserData />
+      </Suspense>
+    </Card>
+  )
 }
