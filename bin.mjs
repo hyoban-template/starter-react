e6b1700319d5349826bf4fd2a1d771158079331b
Stephen Zhou <hi@hyoban.cc>
2024-01-19 03:10:44 +0800

    build: use nitropack
    Part of Y-149

---
+++ bin.mjs
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import "dotenv/config"
+
+await import("./.output/server/index.mjs")
--- vite.config.ts
+++ vite.config.ts
@@ -1,65 +1,10 @@
-import fs from "node:fs"
-import { dirname, resolve } from "node:path"
-import { fileURLToPath } from "node:url"
 import React from "@vitejs/plugin-react-swc"
 import UnoCSS from "unocss/vite"
 import { defineConfig, loadEnv } from "vite"
+import NitroDeploy from "vite-plugin-nitro-deploy"
+import TsconfigPaths from "vite-tsconfig-paths"
 
-import type { Plugin } from "vite"
-
-const _dirname =
-  typeof __dirname !== "undefined"
-    ? __dirname
-    : dirname(fileURLToPath(import.meta.url))
-
-type ViteProxyPluginOptions = {
-  proxyPath?: string
-  proxyTarget?: string
-}
-
-function ViteProxyPlugin(options?: ViteProxyPluginOptions): Plugin {
-  const { proxyPath = "/api", proxyTarget = "http://localhost:3000" } =
-    options ?? {}
-
-  return {
-    name: "vite-proxy-plugin",
-    apply: "build",
-    closeBundle: () => {
-      fs.writeFileSync(
-        "nginx.conf",
-        `
-server {
-  listen 80;
-
-  root /usr/share/nginx/html;
-  index index.html;
-
-  # 开启 gzip 压缩
-  gzip on;
-  gzip_types text/plain text/css application/javascript application/json;
-
-  location ^~${proxyPath}/ {
-    rewrite ^/(.*)$ /$1 break;
-    proxy_pass ${proxyTarget};
-    proxy_http_version 1.1;
-  }
-
-  location / {
-    try_files $uri $uri/ /index.html =404;
-  }
-}`,
-        "utf-8",
-      )
-    },
-  }
-}
-
-const env = loadEnv(
-  process.env["RAILWAY_ENVIRONMENT_NAME"] ??
-    process.env["NODE_ENV"] ??
-    "development",
-  process.cwd(),
-)
+const env = loadEnv(process.env["NODE_ENV"] ?? "development", process.cwd())
 
 const proxyPath = env["VITE_PROXY_PATH"]
 const proxyTarget = env["VITE_PROXY_TARGET"]
@@ -70,6 +15,7 @@ if (!proxyPath || !proxyTarget) {
 
 export default defineConfig({
   plugins: [
+    TsconfigPaths(),
     React({
       plugins: [
         [
@@ -87,16 +33,12 @@ export default defineConfig({
       ],
     }),
     UnoCSS(),
-    ViteProxyPlugin({
-      proxyPath,
-      proxyTarget,
+    NitroDeploy({
+      nitro: {
+        enabled: true,
+      },
     }),
   ],
-  resolve: {
-    alias: {
-      "~": resolve(_dirname, "./src"),
-    },
-  },
   server: {
     proxy: {
       [proxyPath]: {
