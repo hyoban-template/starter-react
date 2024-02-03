bf5f9da5daa487c2da45673bf13bf50e498063fb
Stephen Zhou <hi@hyoban.cc>
2024-02-03 18:05:59 +0800

    chore: check env for build

---
+++ src/env.ts
@@ -0,0 +1,12 @@
+import { createEnv } from "@t3-oss/env-core"
+import { z } from "zod"
+
+export const env = createEnv({
+  clientPrefix: "VITE_",
+  client: {
+    VITE_PROXY_PATH: z.string(),
+    VITE_PROXY_TARGET: z.string(),
+  },
+  runtimeEnv: import.meta.env,
+  emptyStringAsUndefined: true,
+})
--- vite.config.ts
+++ vite.config.ts
@@ -5,66 +5,67 @@ import Checker from "vite-plugin-checker"
 import NitroDeploy from "vite-plugin-nitro-deploy"
 import TsconfigPaths from "vite-tsconfig-paths"
 
-const env = loadEnv(process.env["NODE_ENV"] ?? "development", process.cwd())
+export default defineConfig(async ({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  // @ts-expect-error
+  import.meta.env = env
+  await import("./src/env")
 
-const proxyPath = env["VITE_PROXY_PATH"]
-const proxyTarget = env["VITE_PROXY_TARGET"]
+  const proxyPath = env["VITE_PROXY_PATH"]
+  const proxyTarget = env["VITE_PROXY_TARGET"]
 
-if (!proxyPath || !proxyTarget) {
-  throw new Error("VITE_PROXY_PATH or VITE_PROXY_TARGET is not set")
-}
-
-export default defineConfig({
-  plugins: [
-    TsconfigPaths(),
-    React({
-      babel: {
-        presets: [
-          [
-            "jotai/babel/preset",
-            {
-              customAtomNames: [
-                "atomDark",
-                "atomWithQuery",
-                "atomWithSuspenseQuery",
-                "atomWithLocation",
-              ],
-            },
+  return {
+    plugins: [
+      TsconfigPaths(),
+      React({
+        babel: {
+          presets: [
+            [
+              "jotai/babel/preset",
+              {
+                customAtomNames: [
+                  "atomDark",
+                  "atomWithQuery",
+                  "atomWithSuspenseQuery",
+                  "atomWithLocation",
+                ],
+              },
+            ],
           ],
-        ],
-      },
-    }),
-    UnoCSS(),
-    NitroDeploy({
-      nitro: {
-        enabled: true,
-      },
-    }),
-    Checker({
-      typescript: true,
-      eslint: {
-        lintCommand: "eslint --max-warnings 0 .",
-        useFlatConfig: true,
-      },
-    }),
-  ],
-  server: {
-    proxy: {
-      [proxyPath]: {
-        target: proxyTarget,
-        secure: false,
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom", "react-router-dom"],
-          zod: ["zod"],
+        },
+      }),
+      UnoCSS(),
+      NitroDeploy({
+        nitro: {
+          enabled: true,
+        },
+      }),
+      Checker({
+        typescript: true,
+        eslint: {
+          lintCommand: "eslint --max-warnings 0 .",
+          useFlatConfig: true,
+        },
+      }),
+    ],
+    server: {
+      proxy: {
+        [proxyPath]: {
+          target: proxyTarget,
+          secure: false,
+          changeOrigin: true,
         },
       },
     },
-  },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ["react", "react-dom", "react-router-dom"],
+            zod: ["zod"],
+          },
+        },
+      },
+    },
+  }
 })
