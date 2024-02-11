63e43ae78dc55e5c783fc2b3ab24a26128e5abea
Stephen Zhou <hi@hyoban.cc>
2024-02-11 21:16:18 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import React from "@vitejs/plugin-react"
 import UnoCSS from "unocss/vite"
 import { defineConfig, loadEnv } from "vite"
-import Checker from "vite-plugin-checker"
 import NitroDeploy from "vite-plugin-nitro-deploy"
 import TsconfigPaths from "vite-tsconfig-paths"
 
@@ -38,13 +37,6 @@ export default defineConfig(async ({ mode }) => {
       NitroDeploy({
         nitro: {
           enabled: true,
-        },
-      }),
-      Checker({
-        typescript: true,
-        eslint: {
-          lintCommand: "eslint --max-warnings 0 .",
-          useFlatConfig: true,
         },
       }),
     ],
