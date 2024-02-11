2b3ee36efd35ab5264c46b3d1e532a408a1aff8d
Stephen Zhou <hi@hyoban.cc>
2024-02-11 21:12:21 +0800

    chore: update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,47 +3,33 @@ import "virtual:uno.css"
 import "~/lib/i18n"
 
 import { Provider } from "jotai"
-import { DevTools } from "jotai-devtools"
 import { Toaster } from "sonner"
 
 import { TailwindIndicator } from "~/components/tailwind-indicator"
 import { useDark } from "~/hooks/use-dark"
-import { cn } from "~/lib/utils"
 import { store } from "~/store"
 
 export default function Layout({ children }: React.PropsWithChildren) {
   const { theme } = useDark()
   return (
     <Provider store={store}>
-      <div
-        className={cn(
-          "h-full",
-          "[&_.jotai-devtools-trigger-button]:left-unset",
-          "[&_.jotai-devtools-trigger-button]:right-10 [&_.jotai-devtools-trigger-button]:bottom-8",
-          "[&_.jotai-devtools-trigger-button]:size-12",
-          "[&_.jotai-devtools-trigger-button]:bg-transparent",
-          "[&_.jotai-devtools-shell]:max-w-7xl",
-        )}
-      >
-        {children}
-        <Toaster
-          theme={theme}
-          className="toaster group"
-          toastOptions={{
-            classNames: {
-              toast:
-                "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
-              description: "group-[.toast]:text-muted-foreground",
-              actionButton:
-                "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
-              cancelButton:
-                "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
-            },
-          }}
-        />
-        <TailwindIndicator />
-        <DevTools theme={theme} />
-      </div>
+      {children}
+      <Toaster
+        theme={theme}
+        className="toaster group"
+        toastOptions={{
+          classNames: {
+            toast:
+              "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            description: "group-[.toast]:text-muted-foreground",
+            actionButton:
+              "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
+            cancelButton:
+              "group-[.toast]:bg-muted group-[.toast]:text-muted-foreground",
+          },
+        }}
+      />
+      <TailwindIndicator />
     </Provider>
   )
 }
