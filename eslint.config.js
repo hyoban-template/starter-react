a79e9bedd3239e7e7aa9421c7d9a13f5a8031108
Stephen Zhou <hi@hyoban.cc>
2024-02-13 23:42:35 +0800

    chore: update

--- eslint.config.js
+++ eslint.config.js
@@ -47,7 +47,6 @@ export default config(
         'unicorn/no-null': 'off',
         // https://github.com/orgs/web-infra-dev/discussions/10
         'unicorn/prefer-top-level-await': 'off',
-        'unicorn/no-array-reduce': 'off',
       },
     },
   ],
