1ca6736a2a6d0051c618e8cee8f87fc73ee6beff
Stephen Zhou <hi@hyoban.cc>
2024-05-15 05:41:01 +0800

    chore: knip

--- src/store/location.ts
+++ src/store/location.ts
@@ -2,7 +2,7 @@ import { atomWithLocation } from 'jotai-location'
 
 import { store } from '.'
 
-export const locationAtom = atomWithLocation({ replace: true })
+const locationAtom = atomWithLocation({ replace: true })
 
 export function goToLogin() {
   store.set(locationAtom, {
