cca07b899a44d3f16ced0a23f0eb5af8e391d3fc
Stephen Zhou <hi@hyoban.cc>
2024-05-15 14:25:28 +0800

    chore: update

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,23 +4,16 @@ import { useTranslation } from 'react-i18next'
 import { navigate } from 'wouter/use-browser-location'
 import { z } from 'zod'
 
-import { MyInput } from '~/components/my-input'
 import { Button } from '~/components/ui/button'
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '~/components/ui/form'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '~/components/ui/card'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '~/components/ui/form'
+import { Input } from '~/components/ui/input'
 import { Utility } from '~/components/utility'
 import { myFetch } from '~/lib/network'
 
 const loginInputSchema = z.object({
   username: z.string().min(1),
   password: z.string().min(1),
-  remember: z.boolean(),
 })
 type LoginInput = z.infer<typeof loginInputSchema>
 interface LoginOutput { [key: string]: unknown, token: string }
@@ -32,70 +25,58 @@ export function LoginPage() {
     defaultValues: {
       username: '',
       password: '',
-      remember: false,
     },
   })
 
-  const onSubmit = form.handleSubmit((data) => {
-    void (async () => {
-      try {
-        await myFetch<LoginOutput, LoginInput>(['/api/login', data])
-        navigate('/', { replace: true })
-      }
-      catch (err) {
-        console.error(err)
-      }
-    })()
-  })
+  const onSubmit = async (data: LoginInput) => {
+    try {
+      await myFetch<LoginOutput, LoginInput>(['/api/login', data])
+    }
+    catch (err) {
+      console.error(err)
+    }
+    finally {
+      navigate('/', { replace: true })
+    }
+  }
 
   return (
-    <>
-      <h2 className="text-2xl font-bold text-center mb-4">
-        {t('auth.header')}
-      </h2>
-      <Form {...form}>
-        <form onSubmit={onSubmit} className="space-y-8 min-w-18rem">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>{t('auth.username')}</FormLabel>
-                <FormControl>
-                  <MyInput
-                    iconClassName="i-lucide-user"
-                    placeholder={t('auth.input-username')}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>{t('auth.password')}</FormLabel>
-                <FormControl>
-                  <MyInput
-                    iconClassName="i-lucide-key"
-                    type="password"
-                    placeholder={t('auth.input-password')}
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="w-full">
-            {t('auth.login')}
-          </Button>
-        </form>
-      </Form>
-      <Utility className="mt-6" />
-    </>
+    <Card className="w-full w-[max(100%,_20rem)] max-w-sm">
+      <CardHeader>
+        <CardTitle className="text-2xl">{t('auth.header')}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-4">
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem className="grid gap-2">
+                  <FormLabel>{t('auth.username')}</FormLabel>
+                  <FormControl><Input placeholder={t('auth.input-username')} {...field} /></FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem className="grid gap-2">
+                  <FormLabel>{t('auth.password')}</FormLabel>
+                  <FormControl><Input type="password" placeholder={t('auth.input-password')} {...field} /></FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit" className="w-full">{t('auth.login')}</Button>
+          </form>
+        </Form>
+      </CardContent>
+      <CardFooter>
+        <Utility className="mx-auto" />
+      </CardFooter>
+    </Card>
   )
 }
