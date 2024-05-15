fc6b68ea280361cff23b9d87ea043a7574f0cf23
Stephen Zhou <hi@hyoban.cc>
2024-05-15 14:33:40 +0800

    chore: update

---
+++ src/components/input-with-icon.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react'
+
+import { cn } from '~/lib/utils'
+
+import type { InputProps } from './ui/input'
+import { Input } from './ui/input'
+
+export type MyInputProps = { iconClassName?: string | undefined } & InputProps
+
+/**
+ * @public
+ */
+export const InputWithIcon = React.forwardRef<HTMLInputElement, MyInputProps>(
+  ({ className, iconClassName, ...props }, ref) => {
+    if (!iconClassName)
+      return <Input className={className} ref={ref} {...props} />
+
+    return (
+      <div className={cn('relative w-full', className)}>
+        <Input className="pl-9" ref={ref} {...props} />
+        <span
+          className={cn(
+            'text-muted-foreground absolute inset-0 top-1/2 -translate-y-1/2 left-2',
+            iconClassName,
+          )}
+        />
+      </div>
+    )
+  },
+)
