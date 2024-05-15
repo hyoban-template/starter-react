22bc35970a70c7f6de7234f70d48f06cce6c819d
Stephen Zhou <hi@hyoban.cc>
2024-05-15 15:53:01 +0800

    chore: update

--- src/components/appearance-switch.tsx
+++ src/components/appearance-switch.tsx
@@ -1,24 +1,19 @@
 import { useTranslation } from 'react-i18next'
 
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from '~/components/ui/tooltip'
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '~/components/ui/tooltip'
 import { useDark } from '~/hooks/use-dark'
 import { cn } from '~/lib/utils'
 
 export function AppearanceSwitch({ className }: { className?: string }) {
-  const { toggleDark } = useDark()
   const { t } = useTranslation()
+  const { toggleDark } = useDark()
 
   return (
     <TooltipProvider>
       <Tooltip>
         <TooltipTrigger onClick={toggleDark} className={cn('flex', className)}>
-          <div className="i-lucide-sun scale-100 dark:scale-0 transition-transform duration-500 rotate-0 dark:-rotate-90" />
-          <div className="i-lucide-moon absolute scale-0 dark:scale-100 transition-transform duration-500 rotate-90 dark:rotate-0" />
+          <div className="i-lucide-sun scale-100 dark:scale-0 transition-transform duration-500 rotate-0 dark:-rotate-90" role="img" aria-hidden="true" />
+          <div className="i-lucide-moon absolute scale-0 dark:scale-100 transition-transform duration-500 rotate-90 dark:rotate-0" role="img" aria-hidden="true" />
         </TooltipTrigger>
         <TooltipContent>{t('common.change-theme')}</TooltipContent>
       </Tooltip>
