c5761bdba4ad34138cebaa824547b64c7d2a382f
Stephen Zhou <hi@hyoban.cc>
2024-05-15 16:14:32 +0800

    chore: patch wouter

--- src/store/route.ts
+++ src/store/route.ts
@@ -1,24 +1,4 @@
 import { atomWithLocation } from 'jotai-location'
-
-const eventPopstate = 'popstate'
-const eventPushState = 'pushState'
-const eventReplaceState = 'replaceState'
-const eventHashchange = 'hashchange'
-const events = [
-  eventPopstate,
-  eventPushState,
-  eventReplaceState,
-  eventHashchange,
-]
-
-function subscribeToLocationUpdates(callback: () => void) {
-  for (const event of events)
-    addEventListener(event, callback)
-
-  return () => {
-    for (const event of events)
-      removeEventListener(event, callback)
-  }
-}
+import { subscribeToLocationUpdates } from 'wouter/use-browser-location'
 
 export const locationAtom = atomWithLocation({ subscribe: subscribeToLocationUpdates })
